553feea62a27a805cd199b8948d459f638bd97c0
littleniannian <jarviscrick@outlook.com>
2025-12-26 16:27:23 +0800

    fix: SQLServerConnection cast exception and sql query

--- server/plugins/connect-plugin-sqlserver/src/main/java/com/oceanbase/odc/plugin/connect/sqlserver/SqlServerSessionExtension.java
+++ server/plugins/connect-plugin-sqlserver/src/main/java/com/oceanbase/odc/plugin/connect/sqlserver/SqlServerSessionExtension.java
@@ -24,11 +24,15 @@ import com.oceanbase.odc.common.util.JdbcOperationsUtil;
 import com.oceanbase.odc.plugin.connect.model.DBClientInfo;
 import com.oceanbase.odc.plugin.connect.obmysql.OBMySQLSessionExtension;
 
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author yizhou.xw
  * @date 2024/12
  * @since ODC_release_4.3.4
  */
+@Slf4j
 @Extension
 public class SqlServerSessionExtension extends OBMySQLSessionExtension {
 
@@ -53,6 +57,39 @@ public class SqlServerSessionExtension extends OBMySQLSessionExtension {
             // Fallback to default schema
             return "dbo";
         }
+    }
+
+    @Override
+    public String getConnectionId(Connection connection) {
+        // SQL Server uses @@SPID to get the session process ID
+        String querySql = "SELECT @@SPID";
+        try {
+            return JdbcOperationsUtil.getJdbcOperations(connection).queryForObject(querySql, String.class);
+        } catch (Exception e) {
+            log.warn("Failed to get connection ID from SQL Server using @@SPID", e);
+            // Fallback: try to get connection ID from sys.dm_exec_sessions
+            try {
+                querySql = "SELECT session_id FROM sys.dm_exec_sessions WHERE session_id = @@SPID";
+                return JdbcOperationsUtil.getJdbcOperations(connection).queryForObject(querySql, String.class);
+            } catch (Exception ex) {
+                log.warn("Failed to get connection ID from sys.dm_exec_sessions", ex);
+                // Return empty string as fallback
+                return "";
+            }
+        }
+    }
+
+    @Override
+    public String getKillQuerySql(@NonNull String connectionId) {
+        // SQL Server doesn't have a separate KILL QUERY command
+        // Use KILL to terminate the session (which will also kill the query)
+        return getKillSessionSql(connectionId);
+    }
+
+    @Override
+    public String getKillSessionSql(@NonNull String connectionId) {
+        // SQL Server uses KILL <session_id> to terminate a session
+        return "KILL " + connectionId;
     }
 
     @Override
