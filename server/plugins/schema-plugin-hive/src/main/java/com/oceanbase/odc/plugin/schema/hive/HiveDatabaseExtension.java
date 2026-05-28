090c935b844cc59977388a46ae8eaac423ce0cba
actiontech-zihan <yangzhongjiao@actionsky.com>
2026-05-28 11:09:41 +0000

    feat(schema-plugin-hive): add Hive schema plugin with database/table/view extensions (#871)
    New PF4J plugin module schema-plugin-hive providing:
    - HiveSchemaPlugin: plugin entry point for DialectType.HIVE
    - DBAccessorUtil: bridge between schema-plugin and db-browser factory system,
      creates HiveSchemaAccessor, HiveStatsAccessor, HiveTableEditor via DBBrowser fluent API
    - HiveDatabaseExtension: extends OBMySQLDatabaseExtension, delegates to Hive schema accessor
    - HiveTableExtension: extends OBMySQLTableExtension, overrides getDetail/getTableEditor/
      getSchemaAccessor/getStatsAccessor with Hive-specific implementations
    - HiveViewExtension: extends OBMySQLViewExtension, uses Hive view template for CREATE VIEW

---
+++ server/plugins/schema-plugin-hive/src/main/java/com/oceanbase/odc/plugin/schema/hive/HiveDatabaseExtension.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2024 OceanBase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.oceanbase.odc.plugin.schema.hive;
+
+import java.sql.Connection;
+
+import org.pf4j.Extension;
+
+import com.oceanbase.odc.plugin.schema.hive.utils.DBAccessorUtil;
+import com.oceanbase.odc.plugin.schema.obmysql.OBMySQLDatabaseExtension;
+import com.oceanbase.tools.dbbrowser.schema.DBSchemaAccessor;
+
+/**
+ * Hive database extension. Delegates to the Hive schema accessor via DBAccessorUtil.
+ *
+ * @since ODC_release_4.3.4
+ */
+@Extension
+public class HiveDatabaseExtension extends OBMySQLDatabaseExtension {
+
+    @Override
+    protected DBSchemaAccessor getSchemaAccessor(Connection connection) {
+        return DBAccessorUtil.getSchemaAccessor(connection);
+    }
+
+}
---
+++ server/plugins/schema-plugin-hive/src/main/java/com/oceanbase/odc/plugin/schema/hive/HiveViewExtension.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024 OceanBase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.oceanbase.odc.plugin.schema.hive;
+
+import java.sql.Connection;
+
+import org.pf4j.Extension;
+
+import com.oceanbase.odc.core.shared.constant.DialectType;
+import com.oceanbase.odc.plugin.schema.hive.utils.DBAccessorUtil;
+import com.oceanbase.odc.plugin.schema.obmysql.OBMySQLViewExtension;
+import com.oceanbase.tools.dbbrowser.DBBrowser;
+import com.oceanbase.tools.dbbrowser.model.DBView;
+import com.oceanbase.tools.dbbrowser.schema.DBSchemaAccessor;
+import com.oceanbase.tools.dbbrowser.template.DBObjectTemplate;
+
+import lombok.NonNull;
+
+/**
+ * Hive view extension. Delegates to the Hive schema accessor and uses
+ * the Hive view template for CREATE VIEW generation.
+ *
+ * @since ODC_release_4.3.4
+ */
+@Extension
+public class HiveViewExtension extends OBMySQLViewExtension {
+
+    @Override
+    protected DBSchemaAccessor getSchemaAccessor(Connection connection) {
+        return DBAccessorUtil.getSchemaAccessor(connection);
+    }
+
+    @Override
+    public String generateCreateTemplate(@NonNull DBView view) {
+        return getTemplate().generateCreateObjectTemplate(view);
+    }
+
+    @Override
+    protected DBObjectTemplate<DBView> getTemplate() {
+        return DBBrowser.objectTemplate().viewTemplate()
+                .setType(DialectType.HIVE.getDBBrowserDialectTypeName()).create();
+    }
+
+}
